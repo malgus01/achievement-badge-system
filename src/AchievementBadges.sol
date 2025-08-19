1dfe74ea94c3a51fbda1e878cd6baa8ed92777cd
hectiik <mlghectiik@gmail.com>
2025-08-19 14:41:54 -0400

    Feat: Add Necessary imports

--- src/AchievementBadges.sol
+++ src/AchievementBadges.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract AchievementBadges {}
