c72d38a16162db80dd06b7834e35f2c95af410b0
hectiik <mlghectiik@gmail.com>
2025-08-24 10:55:10 -0400

    Feat: Add tokenURI Public Override Func

--- src/AchievementBadges.sol
+++ src/AchievementBadges.sol
@@ -192,4 +192,8 @@ contract AchievementBadge is ERC721, ERC721URIStorage, Ownable {
             }
         }
     }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
 }
