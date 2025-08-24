6758c371720dda8b3505c6bbbce12647ff36fa77
hectiik <mlghectiik@gmail.com>
2025-08-24 10:56:12 -0400

    Feat: Add Required Override Natspec Comments

--- src/AchievementBadges.sol
+++ src/AchievementBadges.sol
@@ -193,6 +193,7 @@ contract AchievementBadge is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
+    // Override required by Solidity for multiple inheritance
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
