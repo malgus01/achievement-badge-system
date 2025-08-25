a075a05acbc17457e3c2edf0b036731d66e583a7
hectiik <mlghectiik@gmail.com>
2025-08-25 00:42:47 -0400

    Feat: Add Necessary Imports

--- src/AchievementManager.sol
+++ src/AchievementManager.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./AchievementBadge.sol";
+import "./IActivityTracker.sol";
+
 /**
  * @title AchievementManager
  * @dev Central manager for all achievements and badge minting
