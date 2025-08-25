fee6796a804fe4c075596efd8aaab01f2ce370df
hectiik <mlghectiik@gmail.com>
2025-08-25 00:42:30 -0400

    Feat: Add AchievementManager Contract Title Natspec Comments

--- src/AchievementManager.sol
+++ src/AchievementManager.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * @title AchievementManager
+ * @dev Central manager for all achievements and badge minting
+ * @author MLGHECTIIK
+ */
 contract AchievementManager {}
