1531653bcdf6009f7626b0771803e6b249d4852f
malgus01 <mlghectiik@gmail.com>
2025-09-13 17:41:53 -0700

    feat: Add recordActivity External Function Natspec Comments & forge fmt

--- src/IActivityTracker.sol
+++ src/IActivityTracker.sol
@@ -7,10 +7,15 @@ pragma solidity ^0.8.19;
  * @author Your Name
  */
 interface IActivityTracker {
-        // Events
+    // Events
     event ActivityRecorded(address indexed user, bytes32 indexed activityType, uint256 value, uint256 timestamp);
-        event UserStatsUpdated(address indexed user, bytes32 indexed statType, uint256 oldValue, uint256 newValue);
-    
-    function recordActivity(address user, bytes32 activityType, uint256 value) external;
+    event UserStatsUpdated(address indexed user, bytes32 indexed statType, uint256 oldValue, uint256 newValue);
 
+    /**
+     * @dev Record a new activity for a user
+     * @param user Address of the user
+     * @param activityType Type of activity (hashed string)
+     * @param value Value associated with the activity
+     */
+    function recordActivity(address user, bytes32 activityType, uint256 value) external;
 }
