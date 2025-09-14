357d0c4c431b07d199102b12c6e48505692c06dc
malgus01 <mlghectiik@gmail.com>
2025-09-13 17:40:15 -0700

    feat: Add ActivityRecorded Event

--- src/IActivityTracker.sol
+++ src/IActivityTracker.sol
@@ -6,4 +6,8 @@ pragma solidity ^0.8.19;
  * @dev Interface for activity tracking contracts
  * @author Your Name
  */
-interface IActivityTracker {}
+interface IActivityTracker {
+        // Events
+    event ActivityRecorded(address indexed user, bytes32 indexed activityType, uint256 value, uint256 timestamp);
+    
+}
