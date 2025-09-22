6d683baa09668127c50081caab0f8acb2e5b2fc5
malgus01 <mlghectiik@gmail.com>
2025-09-21 21:58:36 -0700

    feat: Add Interface folder

--- src/AchievementManager.sol
+++ src/AchievementManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./AchievementBadges.sol";
-import "./IActivityTracker.sol";
+import "./interface/IActivityTracker.sol";
 
 /**
  * @title AchievementManager
