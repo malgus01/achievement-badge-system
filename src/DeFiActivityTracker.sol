33b660e124336f2a4b7406652b585b250f53c688
malgus01 <mlghectiik@gmail.com>
2025-09-21 22:02:01 -0700

    feat: Add Contract Natspec Title Comments

--- src/DeFiActivityTracker.sol
+++ src/DeFiActivityTracker.sol
@@ -6,6 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interface/IActivityTracker.sol";
 
+/**
+ * @title DeFiActivityTracker
+ * @dev Tracks DeFi-related activities like swaps, liquidity provision, lending, etc.
+ */
 contract DeFiActivityTracker {
 }
 
