e6d8a8513594b635d2ee0c693cb50afae50ee819
malgus01 <mlghectiik@gmail.com>
2025-09-24 10:21:44 -0700

    feat: Celo Mainnet Deployments

--- src/DeFiActivityTracker.sol
+++ src/DeFiActivityTracker.sol
@@ -9,12 +9,12 @@ import "./interface/IActivityTracker.sol";
  * @title DeFiActivityTracker
  * @dev Tracks DeFi-related activities like swaps, liquidity provision, lending, etc.
  */
-contract DeFiActivityTracker is IActivityTracker, Ownable, ReentrancyGuard {
-    // Activity type constants
-    bytes32 public constant SWAP = keccak256("SWAP");
-    bytes32 public constant LIQUIDITY_ADD = keccak256("LIQUIDITY_ADD");
-    bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
-    bytes32 public constant LENDING = keccak256("LENDING");
+// contract DeFiActivityTracker is IActivityTracker, Ownable, ReentrancyGuard {
+//     // Activity type constants
+//     bytes32 public constant SWAP = keccak256("SWAP");
+//     bytes32 public constant LIQUIDITY_ADD = keccak256("LIQUIDITY_ADD");
+//     bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
+//     bytes32 public constant LENDING = keccak256("LENDING");
 
-    constructor(address initialOwner) Ownable(initialOwner) {}
-}
+//     constructor(address initialOwner) Ownable(initialOwner) {}
+// }
