141400e81cce6137021b269dd17662fcfe6e4c54
malgus01 <mlghectiik@gmail.com>
2025-09-24 10:23:00 -0700

    feat: Add bytes32 public constant BORROWING

--- src/DeFiActivityTracker.sol
+++ src/DeFiActivityTracker.sol
@@ -9,12 +9,13 @@ import "./interface/IActivityTracker.sol";
  * @title DeFiActivityTracker
  * @dev Tracks DeFi-related activities like swaps, liquidity provision, lending, etc.
  */
-// contract DeFiActivityTracker is IActivityTracker, Ownable, ReentrancyGuard {
-//     // Activity type constants
-//     bytes32 public constant SWAP = keccak256("SWAP");
-//     bytes32 public constant LIQUIDITY_ADD = keccak256("LIQUIDITY_ADD");
-//     bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
-//     bytes32 public constant LENDING = keccak256("LENDING");
+contract DeFiActivityTracker is IActivityTracker, Ownable, ReentrancyGuard {
+    // Activity type constants
+    bytes32 public constant SWAP = keccak256("SWAP");
+    bytes32 public constant LIQUIDITY_ADD = keccak256("LIQUIDITY_ADD");
+    bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
+    bytes32 public constant LENDING = keccak256("LENDING");
+    bytes32 public constant BORROWING = keccak256("BORROWING");
 
-//     constructor(address initialOwner) Ownable(initialOwner) {}
-// }
+    constructor(address initialOwner) Ownable(initialOwner) {}
+}
